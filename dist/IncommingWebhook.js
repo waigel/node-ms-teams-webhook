696fbddae06e339bfc0e7ab1d284a4e5fae7aaf1
Johannes Waigel <johannes@waigel.me>
2021-01-06 02:44:14 +0100

    security - upgrade axios to v0.21.1
    Co-authored-by: Valentnin Sickert <lapotor@lapotor.de>

--- dist/IncommingWebhook.js
+++ dist/IncommingWebhook.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.IncomingWebhook = void 0;
 const axios_1 = __importDefault(require("axios"));
 /**
  * A client for Slack's Incoming Webhooks
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 /// <reference lib="es2017" />
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.IncomingWebhook = void 0;
 var IncommingWebhook_1 = require("./IncommingWebhook");
-exports.IncomingWebhook = IncommingWebhook_1.IncomingWebhook;
+Object.defineProperty(exports, "IncomingWebhook", { enumerable: true, get: function () { return IncommingWebhook_1.IncomingWebhook; } });
 //# sourceMappingURL=index.js.map
