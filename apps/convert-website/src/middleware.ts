81ca9b106f35250ebaf5b20769e3bff7b51a5e88
waigel <johannes@waigel.me>
2024-08-26 02:51:58 +0200

    chore: lint again (#35)

--- apps/convert-website/src/middleware.ts
+++ apps/convert-website/src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { kv } from "@vercel/kv";
+import { NextRequest, NextResponse } from "next/server";
 
 const ratelimit = new Ratelimit({
   redis: kv,
